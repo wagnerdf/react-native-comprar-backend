4e9dcd3bc0f5a15a2d32713d15f1ddaae06950db
Wagner Andrade <Wagnerandrade_df@hotmail.com>
2026-06-18 15:20:27 -0300

    feat: add user service with user and auth creation logic

---
+++ src/main/java/com/wagnerdf/comprar/service/UserService.java
@@ -0,0 +1,62 @@
+package com.wagnerdf.comprar.service;
+
+import java.time.LocalDateTime;
+
+import org.springframework.stereotype.Service;
+
+import com.wagnerdf.comprar.entity.Auth;
+import com.wagnerdf.comprar.entity.User;
+import com.wagnerdf.comprar.repository.AuthRepository;
+import com.wagnerdf.comprar.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+	
+	private final UserRepository userRepository;
+	private final AuthRepository authRepository;
+	
+	public void createUser(User user, String username, String password ) {
+		
+		// validar se o email já existe
+		userRepository.findByEmail(user.getEmail())
+			.ifPresent(u ->{
+				throw new RuntimeException("Email já cadastrado");
+			});
+		
+		// validar se o username já existe
+		authRepository.findByUsername(username)
+			.ifPresent(u ->{
+				throw new RuntimeException("Username já cadastrado");
+			});
+		
+		// preencher datas
+        user.setCreatedAt(LocalDateTime.now());
+        user.setUpdatedAt(LocalDateTime.now());
+        
+        // salvar usuário
+        User savedUser = userRepository.save(user);
+        
+        // criar auth
+        Auth auth = Auth.builder()
+        		.user(savedUser)
+        		.username(username)
+        		.password(password)
+        		.build();
+        
+        // salvar auth
+        authRepository.save(auth);
+		
+	}
+}
+
+
+
+
+
+
+
+
+
