83e4af9657586fd1b13c842c03bdd1d5ed090f77
Wagner Andrade <Wagnerandrade_df@hotmail.com>
2026-06-27 19:14:42 -0300

    test(user): update user creation test

--- src/test/java/com/wagnerdf/comprar/service/UserServiceTest.java
+++ src/test/java/com/wagnerdf/comprar/service/UserServiceTest.java
@@ -1,13 +1,17 @@
 package com.wagnerdf.comprar.service;
 
-import com.wagnerdf.comprar.entity.User;
+import com.wagnerdf.comprar.dto.request.RegisterRequest;
 import com.wagnerdf.comprar.enums.Gender;
+import com.wagnerdf.comprar.repository.AuthRepository;
+import com.wagnerdf.comprar.repository.UserRepository;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDate;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 @ActiveProfiles("test") 
@@ -15,19 +19,33 @@ class UserServiceTest {
 
     @Autowired
     private UserService userService;
+    
+    @Autowired
+    private UserRepository userRepository;
+    
+    @Autowired
+    private AuthRepository authRepository;
 
     @Test
     void shouldCreateUserSuccessfully() {
 
-        User user = User.builder()
-                .name("Wagner Test")
-                .email("wagner.test@email.com")
-                .birthDate(LocalDate.of(1990, 1, 1))
-                .gender(Gender.MALE)
-                .build();
+    	RegisterRequest request = new RegisterRequest();
 
-        userService.createUser(user, "wagner_test", "123456");
+        request.setName("Wagner Test");
+        request.setEmail("wagner.test@email.com");
+        request.setUsername("wagner_test");
+        request.setPassword("123456");
+        request.setBirthDate(LocalDate.of(1990, 1, 1));
+        request.setGender(Gender.MALE);
 
-        System.out.println("Teste executado com sucesso!");
+        userService.createUser(request);
+
+        boolean userEmailExists =
+                userRepository.findByEmail("wagner.test@email.com").isPresent();
+        assertTrue(userEmailExists);
+        
+        boolean userNameExists =
+        		authRepository.findByUsername("wagner_test").isPresent();
+        assertTrue(userNameExists);
     }
 }
