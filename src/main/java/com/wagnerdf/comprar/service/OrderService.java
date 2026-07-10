0fbd97c918b1a48215d6e38a50f4318a03672490
Wagner Andrade <Wagnerandrade_df@hotmail.com>
2026-07-10 11:34:24 -0300

    feat(order): validate products before order creation

--- src/main/java/com/wagnerdf/comprar/service/OrderService.java
+++ src/main/java/com/wagnerdf/comprar/service/OrderService.java
@@ -2,10 +2,68 @@ package com.wagnerdf.comprar.service;
 
 import org.springframework.stereotype.Service;
 
+import com.wagnerdf.comprar.dto.request.CreateOrderItemRequest;
+import com.wagnerdf.comprar.dto.request.CreateOrderRequest;
+import com.wagnerdf.comprar.entity.Product;
+import com.wagnerdf.comprar.exception.BusinessException;
+import com.wagnerdf.comprar.exception.ProductNotFoundException;
+import com.wagnerdf.comprar.repository.OrderRepository;
+import com.wagnerdf.comprar.repository.ProductRepository;
+import com.wagnerdf.comprar.repository.UserRepository;
+
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class OrderService {
+	
+	private final OrderRepository orderRepository;
+	private final ProductRepository productRepository;
+    
+    public void createOrder(CreateOrderRequest request) {
+    	
+    	 for (CreateOrderItemRequest itemRequest : request.getItems()) {
+
+    	        Product product = validateProduct(itemRequest);
+
+    	        // criar o orderItem e calcular subtotal
+   	
+    	
+    	}
+
+    }
+    
+    private Product validateProduct(CreateOrderItemRequest itemRequest) {
+
+    	// =========================
+        // Localizar produto
+        // =========================
+        Product product = productRepository
+                .findById(itemRequest.getProductId())
+                .orElseThrow(() ->
+                        new ProductNotFoundException("Produto não encontrado."));
+
+        // =========================
+        // Validar produto ativo
+        // =========================
+        if (!product.getActive()) {
+            throw new BusinessException(
+                    "Produto está inativo."
+            );
+        }
+
+        // =========================
+        // Validar estoque
+        // =========================
+        if (product.getStock() < itemRequest.getQuantity()) {
+            throw new BusinessException(
+                    "Estoque insuficiente para o produto: "
+                            + product.getName()
+            );
+        }
+
+        return product;
+
+    }
 
 }
