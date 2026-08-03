fe53de9a4f34603f18cf06c611711a5516dc5d19
Wagner Andrade <Wagnerandrade_df@hotmail.com>
2026-08-03 17:19:42 -0300

    fix(repository): change PermissionRepository ID from String to Long

--- src/main/java/com/wagnerdf/comprar/repository/PermissionRepository.java
+++ src/main/java/com/wagnerdf/comprar/repository/PermissionRepository.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PermissionRepository extends JpaRepository<Permission, String> {
+public interface PermissionRepository extends JpaRepository<Permission, Long> {
 
     Optional<Permission> findByName(String name);
 }
